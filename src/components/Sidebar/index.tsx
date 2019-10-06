026d09ec72e06bbd197ab80c38634f539c795d86
Karan Grover <grover.k912@gmail.com>
2019-10-06 14:33:09 +0800

    initial commit

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './sidebar.module.scss';
+
+const Sidebar = () => {
+  return <div></div>;
+};
+
+export default Sidebar;
---
+++ src/views/App.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Home from 'views/Home/Home';
+
+const App: React.FC = () => {
+  return (
+    <Fragment>
+      <Home />
+    </Fragment>
+  );
+};
+
+export default App;
